6544d8cc16329a05f2bfc3c5b5eafd4aa1b3a3ce
alban316 <jason.brugger@gmail.com>
2016-11-17 14:12:39 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Dictionary<int, string> kvp = new Dictionary<int, string>();
+
+            // kvp.Add(1, "Lorem Ipsem");
+
+            // foreach (var item in kvp)
+            // {
+            //     Console.WriteLine(item.Value);
+            // }
+
+            //////////////////////////////////////////
+            //how do I use this???
+            //////////////////////////////////////////
+            //https://dotnet.myget.org/feed/dotnet-core/package/nuget/System.Data.SqlClient/4.4.0-beta-24717-03
+
+
+        }
+    }
+}
